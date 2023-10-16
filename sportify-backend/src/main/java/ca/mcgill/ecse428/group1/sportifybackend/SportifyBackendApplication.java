d6680260f4fc7b213114502025699dc6c273d628
shaun210 <Shaun.soobagrah@mail.mcgill.ca>
2023-10-15 20:49:24 -0400

    created spring boot project

---
+++ sportify-backend/src/main/java/ca/mcgill/ecse428/group1/sportifybackend/SportifyBackendApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.group1.sportifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportifyBackendApplication.class, args);
+	}
+
+}
---
+++ sportify-backend/src/test/java/ca/mcgill/ecse428/group1/sportifybackend/SportifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.group1.sportifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
