d7b5ebe64d3c9be7fb5fe1d1aaf87ac28a86bafe
shaun210 <Shaun.soobagrah@mail.mcgill.ca>
2023-11-04 13:07:47 -0400

    added friend request status enum class

--- sportify-backend/src/main/java/ca/mcgill/ecse428/group1/sportifybackend/controller/FriendRequestController.java
+++ sportify-backend/src/main/java/ca/mcgill/ecse428/group1/sportifybackend/controller/FriendRequestController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.util.HtmlUtils;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 
 import ca.mcgill.ecse428.group1.sportifybackend.dto.FriendRequestDto;
 import ca.mcgill.ecse428.group1.sportifybackend.service.FriendRequestService;
@@ -26,5 +31,29 @@ public class FriendRequestController {
         return new FriendRequestDto(id);
     }
 
+    
+
+    // @MessageMapping("/chat")
+    // @SendTo("/topic/chat")
+    // public Greeting greeting(HelloMessage message) throws Exception {
+    //   Thread.sleep(1000); // simulated delay
+    //   return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    // }
+    // @MessageMapping("/chat")
+    // @SendTo("/topic/chat")
+    // public String handleMessage(HelloMessage message, WebSocketSession session) {
+    //     // Get the client's IP address from the WebSocketSession
+    //     String clientIpAddress = session.getRemoteAddress().getAddress().getHostAddress();
+    //     System.out.println("sessipn: " + session);
+    //     System.out.println("Received message from client with IP address: " + clientIpAddress);
+    //     // Your message handling logic here
+        
+    //     return "Received message from client with IP address: " + clientIpAddress;
+    // }
+
+    // @RequestMapping("/websocket")
+    // public String getWebSocket() {
+    //     return "ws-broadcast";
+    // }
     
 }
---
+++ sportify-backend/src/main/java/ca/mcgill/ecse428/group1/sportifybackend/model/FriendRequestStatus.java
@@ -0,0 +1,7 @@
+package ca.mcgill.ecse428.group1.sportifybackend.model;
+
+public enum FriendRequestStatus {
+    ACCEPTED,
+    REJECTED,
+    PENDING
+}
